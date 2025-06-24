4914a516852b93c79e82d7f6918594c7073c4cf0
Tp Guerreo <galindezarnaez@gmail.com>
2025-06-24 09:20:45 -0400

    Cambio avanzado

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 console.log('Hola mundo')
 console.log('repositorio iniciado')
+
+
+const n = 22;
+function probando(n) {
+    if (n = 22) {
+        
+        return ('exito');
+    }
+}
+
+console.log(n)
